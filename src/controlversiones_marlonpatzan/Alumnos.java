80a71d91920e2ee678ae29467e306d0db9f723de
wilme <wilme@Wilmer-Patzan14>
2025-11-05 23:16:45 -0600

    Creacion de Alumnos en el menu

---
+++ src/controlversiones_marlonpatzan/Alumnos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlversiones_marlonpatzan;
+
+/**
+ *
+ * @author wilme
+ */
+public class Alumnos {
+    
+}
